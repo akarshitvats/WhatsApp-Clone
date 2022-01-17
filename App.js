6fedd72a63ed5c512cb3b47c42df54a6eaecbd83
Akarshit Vats <90768785+akarshitvats@users.noreply.github.com>
2022-01-17 17:27:35 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import './App.css';
+import Sidebar from './Sidebar';
+import Chat from './Chat';
+import Login from './Login';
+import { BrowserRouter as Router , Route, Link,Switch } from "react-router-dom";
+import {useStateValue} from './stateProvider';
+
+function App() {
+  const [{user}, dispatch] = useStateValue();
+
+  return (
+    <div className="App">
+    {!user ?(
+      <Login />
+    ):(
+      <div className='app__body'>
+      <Router>
+        <Sidebar/>
+        <Switch>
+        <Route path='/rooms/:roomId'>
+          <Chat />
+        </Route>
+          <Route path='/'>
+            <Chat />
+          </Route>
+        </Switch>
+      </Router>
+      </div>
+      )}
+    </div>
+
+  );
+}
+
+export default App;
